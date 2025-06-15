5603af98c6da65c357debd873bdbba7b70d12301
Firmansyah Sundana <88984074+sundana@users.noreply.github.com>
2025-06-15 18:50:07 +0700

    fix flops and params measurement

--- complexity_measurement.py
+++ complexity_measurement.py
@@ -1,7 +1,34 @@
-from math import pow
-from torchinfo import summary
+import torch.nn as nn
+from ptflops import get_model_complexity_info
+import pandas as pd
+
+def calculate_flops(models: list[nn.Module]) -> dict:
+    model_complexity = {
+        "models": [],
+        "flops": [],
+        "params": []
+    }
+    for model in models:
+        model = model(num_classes=29)
+        flop, params = get_model_complexity_info(model, (3, 224, 224), as_strings=True, print_per_layer_stat=False)
+        model_complexity["models"].append(model.__class__.__name__)
+        model_complexity["flops"].append(flop)
+        model_complexity["params"].append(params)
+
+    df = pd.DataFrame(model_complexity)
+    return df
 
 
-def model_summary(model):
-    summary(model)
+if __name__ == "__main__":
+    from my_models import mobilenetv3, proposed_model
+    models = [
+        mobilenetv3.MobileNetV3_Large, 
+        proposed_model.MobileNetV3_Large_CBAM_16,
+        proposed_model.MobileNetV3_Large_CBAM_32,
+        mobilenetv3.MobileNetV3_Small, 
+        proposed_model.MobileNetV3_Small_CBAM_16,
+        proposed_model.MobileNetV3_Small_CBAM_32,
+    ]
+    complexity = calculate_flops(models)
 
+    print(complexity)
