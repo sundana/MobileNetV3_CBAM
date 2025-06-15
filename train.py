965cdf280ae8e69eee2ff414e69bc14643664c08
Firmansyah Sundana <88984074+sundana@users.noreply.github.com>
2025-06-15 22:57:38 +0700

    add train.py argument for training hiperparameter config

--- train.py
+++ train.py
@@ -5,58 +5,84 @@ Trains a PyTorch image classification model using device-agnostic code.
 import torch
 import data_setup
 import engine
-from models.mobilenetv3 import MobileNetV3_Small
+from models.mobilenetv3 import MobileNetV3_Large, MobileNetV3_Small 
+from models.proposed_model import MobileNetV3_Large_CBAM_16, MobileNetV3_Large_CBAM_32, MobileNetV3_Small_CBAM_16, MobileNetV3_Small_CBAM_32
 import utils
 from torchvision import transforms
 
-# Setup hyperparameters
-NUM_EPOCHS = 1
-BATCH_SIZE = 64
-LEARNING_RATE = 0.001
 
-# Setup directories
-data_path = "data/"
+def start_training(model_name: str, num_epochs:int=1, batch_size:int=64, learning_rate:float=0.001):
+    # Setup directories
+    data_path = "data/"
 
-# Setup target device
-if torch.cuda.is_available(): 
-    device = "cuda" 
-elif torch.backends.mps.is_available():
-    device = "mps"
-else: 
-    device = "cpu"
+    # Setup target device
+    if torch.cuda.is_available(): 
+        device = "cuda" 
+    elif torch.backends.mps.is_available():
+        device = "mps"
+    else: 
+        device = "cpu"
 
-# Create transforms
-data_transform = transforms.Compose([
-  transforms.Resize((224, 224)),
-  transforms.ToTensor()
-])
+    # Create transforms
+    data_transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor()
+    ])
 
-# Create DataLoaders with help from data_setup.py
-train_dataloader, val_dataloader, test_dataloader, class_names = data_setup.create_dataloader(
-    data_path=data_path,
-    transform=data_transform,
-    batch_size=BATCH_SIZE
-)
+    # Create DataLoaders with help from data_setup.py
+    train_dataloader, val_dataloader, test_dataloader, class_names = data_setup.create_dataloader(
+        data_path=data_path,
+        transform=data_transform,
+        batch_size=batch_size
+    )
 
-# Create model with help from model_builder.py
-model = MobileNetV3_Small(num_classes=len(class_names)).to(device)
+    if model_name == "mobilenetv3_small":
+        model = MobileNetV3_Small(num_classes=len(class_names)).to(device)
+    elif model_name == "proposed_large_16":
+        model = MobileNetV3_Large_CBAM_16(num_classes=len(class_names)).to(device)
+    elif model_name == "proposed_large_32":
+        model = MobileNetV3_Large_CBAM_32(num_classes=len(class_names)).to(device)
+    elif model_name == "proposed_small_16":
+        model = MobileNetV3_Small_CBAM_16(num_classes=len(class_names)).to(device)
+    elif model_name == "proposed_small_32":
+        model = MobileNetV3_Small_CBAM_32(num_classes=len(class_names)).to(device)
+    else:
+        model = MobileNetV3_Large(num_classes=len(class_names)).to(device)
+    # Create model with help from model_builder.py
 
-# Set loss and optimizer
-loss_fn = torch.nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
+    # Set loss and optimizer
+    loss_fn = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
-# Start training with help from engine.py
-engine.train(model=model,
-             train_dataloader=train_dataloader,
-             valid_dataloader=val_dataloader,
-             loss_fn=loss_fn,
-             optimizer=optimizer,
-             epochs=NUM_EPOCHS,
-             device=device)
+    # Start training with help from engine.py
+    engine.train(model=model,
+                train_dataloader=train_dataloader,
+                valid_dataloader=val_dataloader,
+                loss_fn=loss_fn,
+                optimizer=optimizer,
+                epochs=num_epochs,
+                device=device)
 
-# Save the model with help from utils.py
-utils.save_model(
-    model=model,
-    target_dir="checkpoints",
-    model_name="05_going_modular_script_mode_tinyvgg_model.pth"
-)
+    # Save the model with help from utils.py
+    utils.save_model(
+        model=model,
+        target_dir="checkpoints",
+        model_name="05_going_modular_script_mode_tinyvgg_model.pth"
+    )
+
+    return
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description="Script for train model.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument("-m", "--model", default="mobilenetv3_large", choices=['mobilenetv3_small', 'mobilenetv3_large' 'proposed_large_16', 'proposed_large_32', 'proposed_small_16', 'proposed_small_32'],  help="name of the model")
+    args=parser.parse_args()
+    model_name = args.model
+    print(f"Training model {model_name} start...")
+    start_training(model_name)
+    pass
