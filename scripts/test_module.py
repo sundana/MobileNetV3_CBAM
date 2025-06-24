b6b3da4d32cbb92bf1023a67dabf43aec5270603
Firmansyah Sundana <88984074+sundana@users.noreply.github.com>
2025-06-24 23:28:30 +0700

    fix python package

---
+++ scripts/test_module.py
@@ -0,0 +1,5 @@
+from src.models.cbam import CBAM
+
+
+cbam = CBAM(3)
+print(cbam)
--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,8 @@
 """Main package for the ML project."""
 
 __version__ = "0.1.0"
+
+
+from .models.cbam import CBAM
+
+__all__ = ["CBAM"]
